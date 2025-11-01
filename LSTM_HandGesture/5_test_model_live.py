9c0c7187879d8a1c46041c5dc6664a96dcc06576
zafawniar <zafawniar@gmail.com>
2025-11-01 18:07:41 +0700

    fix(run_model): Optimize FPS

--- LSTM_HandGesture/5_test_model_live.py
+++ LSTM_HandGesture/5_test_model_live.py
@@ -52,6 +52,12 @@ last_sent_action = '...'
 prediction_threshold = 0.97 # threshold disesuaikan sama akurasi model (terakhir 97.9%)
 
 cap = cv2.VideoCapture(0)
+
+# --- PENINGKATAN FPS ---
+# Memaksa kamera menggunakan format stream MJPG
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+# ------------------------
+
 prev_time = 0
 fps = 0
 
