8f72916e6abd49b2c8058865e5aefcd8965b4aae
zafawniar <zafawniar@gmail.com>
2025-11-28 15:16:55 +0700

    feat(esp8266_firmware): expand firmware support for devices 3 and 4

--- esp8266_firmware/relay_esp8266_dynamicIP/relay_esp8266_dynamicIP.ino
+++ esp8266_firmware/relay_esp8266_dynamicIP/relay_esp8266_dynamicIP.ino
@@ -9,7 +9,9 @@ const char* password = "12345678";
 // ESP 1 -> "perangkat-1"
 // ESP 2 -> "perangkat-2"
 // const char* hostName = "perangkat-1"; 
-const char* hostName = "perangkat-2"; 
+// const char* hostName = "perangkat-2"; 
+// const char* hostName = "perangkat-3"; 
+const char* hostName = "perangkat-4"; 
 
 ESP8266WebServer server(80);
 const int relayPin = D1; 
@@ -55,8 +57,10 @@ void setup() {
     // Service: _http._tcp, Name: gesture-iot, Port: 80
     MDNS.addService("http", "tcp", 80);
     MDNS.addServiceTxt("http", "tcp", "type", "gesture-iot"); // Tag khusus kita
-    // MDNS.addServiceTxt("http", "tcp", "id", "1"); // ID perangkat (Ganti jadi "2" di ESP kedua)
-    MDNS.addServiceTxt("http", "tcp", "id", "2"); // ID perangkat (Ganti jadi "2" di ESP kedua)
+    // MDNS.addServiceTxt("http", "tcp", "id", "1"); // ID perangkat 
+    // MDNS.addServiceTxt("http", "tcp", "id", "2"); // ID perangkat 
+    // MDNS.addServiceTxt("http", "tcp", "id", "3"); // ID perangkat 
+    MDNS.addServiceTxt("http", "tcp", "id", "4"); // ID perangkat
   } else {
     Serial.println("Error setting up MDNS responder!");
   }
@@ -72,6 +76,14 @@ void setup() {
   // ESP ini JUGA akan merespon jika dianggap sebagai "Perangkat 2"
   server.on("/21", handleRelayON); 
   server.on("/20", handleRelayOFF);
+
+  // ESP ini JUGA akan merespon jika dianggap sebagai "Perangkat 3"
+  server.on("/31", handleRelayON); 
+  server.on("/30", handleRelayOFF);
+
+  // ESP ini JUGA akan merespon jika dianggap sebagai "Perangkat 4"
+  server.on("/41", handleRelayON); 
+  server.on("/40", handleRelayOFF);
 
   
   server.begin();
